1b1c4bbd2fc945a5d2152ff6054f50dff7c4be6f
Eric Liu <dathtohn@gmail.com>
2016-07-09 13:24:23 -0400

    add updates and more fields

--- pages/home.js
+++ pages/home.js
@@ -1,4 +1,22 @@
 module.exports = {
-  message: 'Welcome! - Eric Liu',
-  routes: ['/', '/resume']
+  context: "I am a 28-year-old software developer living in Brooklyn, NY. While I don't shy away from databases or back-end, my passion lies in delivering a great UX and implementating modern front-end practices.",
+  message: {
+    line1: 'Welcome to my personal site,',
+    line2: 'Visit any of the routes by typing them into the url.',
+    line3: 'Reach me at ericliu121187@gmail.com.',
+    line4: 'Thanks for visiting,',
+    line5: 'Eric'
+  },
+  routes: {
+    '/': 'This is where we are at right now.',
+    '/json': 'This is information about me represented in JSON.',
+    '/resume': 'You can view and download my resume here.'
+  },
+  updates: [
+    {
+      date: '2016-07-08',
+      mood: 'honing react, redux and fiddling around with basic NLP',
+      project: 'i-am-h.com'
+    }
+  ]
 }
